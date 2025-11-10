4bf15c4a6284a8a1c9e1ef88345f62db7cf6d8c0
jjh0420 <ldy060420@gmail.com>
2025-11-10 23:14:13 +0900

    hashmap

---
+++ src/Jjh/Manager2.java
@@ -0,0 +1,111 @@
+package Jjh;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Manager2 {
+    HashMap<String, Word> word = new HashMap<>();
+    static Scanner scanner = new Scanner(System.in);
+
+    void voc(String filename) {
+        try (Scanner file = new Scanner(new File(filename))) {
+
+            while (file.hasNextLine()) {
+                String str = file.nextLine();
+                String[] temp = str.split("\t");
+
+                Word w = new Word(temp[0].trim(), temp[1].trim());
+                word.put(temp[0].trim(), w);
+            }
+
+            System.out.println("단어장 불러오기 완료");
+        } catch (FileNotFoundException e) {
+            System.out.println("파일을 찾을 수 없습니다.");
+        }
+    }
+
+    void save() {
+
+    }
+
+    void correct() {
+        System.out.println("===== 단어 수정 =====");
+        System.out.print("뜻을 수정할 영단어을 입력하세요: ");
+        String eng = scanner.nextLine();
+        if (!word.containsKey(eng)) {
+            System.out.println("해당 단어가 존재하지 않습니다.");
+            return;
+        }
+
+        System.out.print("뜻을 입력하세요: ");
+        String kor = scanner.nextLine();
+
+        Word w = word.get(eng);
+        w.setKor(kor);
+        System.out.println("단어가 수정되었습니다.");
+        //단어 파일 저장
+        save();
+    }
+
+    void delete() {
+        System.out.println("===== 단어 삭제 =====");
+        System.out.print("삭제할 영단어를 입력하세요: ");
+        String eng = scanner.nextLine();
+
+        if (!word.containsKey(eng)) {
+            System.out.println("해당 단어가 존재하지 않습니다.");
+            return;
+        }
+        Word w = word.remove(eng);
+        System.out.println("단어가 삭제되었습니다.");
+        save();
+    }
+
+    void add() {
+        System.out.println("===== 단어 추가 =====");
+        System.out.print("추가할 영단어를 입력하세요: ");
+        String term = scanner.nextLine();
+        if(word.containsKey(term)) {
+            System.out.println("이미 존재하는 영단어입니다.");
+            return;
+        }
+        System.out.print("영단어 뜻을 입력하세요: ");
+        String meaning = scanner.nextLine();
+
+        word.put(term, new Word(term, meaning));
+        System.out.println("단어를 성공적으로 등록했습니다.");
+    }
+
+    void search() {
+        System.out.println("===== 영단어 검색 =====");
+        System.out.print("검색할 단어를 입력하세요(영단어): ");
+        String eng = scanner.nextLine();
+
+        if (!word.containsKey(eng)) {
+            System.out.println("해당 단어가 존재하지 않습니다.");
+            return;
+        }
+        Word w = word.get(eng);
+        System.out.println("뜻 : " + w.getKor());
+    }
+
+    void search2() {
+        System.out.println("===== 영단어 검색 =====");
+        System.out.print("검색할 부분 단어를 입력하세요(영단어): ");
+        String eng = scanner.nextLine();
+
+        boolean found = false;
+        for (Word w : word.values()) {
+            if (w.getEng().startsWith(eng)) {
+                System.out.println(w.getEng() + " = " + w.getKor());
+                found = true;
+            }
+        }
+        if (!found)
+            System.out.println("해당 영단어는 존재하지 않습니다.");
+    }
+
+}
+
