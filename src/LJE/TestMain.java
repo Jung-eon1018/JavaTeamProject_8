2fa93028c85a27dbad7057f12a7e74697df0530f
Jung-Eon <egg9054@gmail.com>
2025-11-13 17:29:39 +0900

    삭제 파일 복구

--- src/LJE/TestMain.java
+++ src/LJE/TestMain.java
@@ -1,7 +1,15 @@
 package LJE;
 
 public class TestMain {
-    static void main() {
-        System.out.println("이정언");
+    public static void main(String[] args) {
+        //작동 Test
+        WrongNotes wrong = new WrongNotes();
+
+        wrong.markWrong("dog","강아지");
+        wrong.markWrong("cat","고양이");
+
+        wrong.save("data/wrong.txt");
+
+        wrong.printAll();
     }
 }
